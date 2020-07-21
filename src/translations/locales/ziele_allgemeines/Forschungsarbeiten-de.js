9fd13f3e0b6f568db4fb93836abbd06e44e595d6
Agnes Seipelt <aseipelt@mail.uni-paderborn.de>
2020-07-21 13:58:08 +0200

    add folder Ziele_allgemeines

---
+++ src/translations/locales/ziele_allgemeines/Forschungsarbeiten-de.js
@@ -0,0 +1,3 @@
+export default {
+(32636940,'<ac:layout><ac:layout-section ac:type=\"two_equal\"><ac:layout-cell><p style=\"text-align: left;\">Die Forschungsarbeiten im ZenMEM befassen sich auf informatischer, technischer und &nbsp;kulturwissenschaftlicher Ebene mit unterschiedlichen Schwerpunkten rund um den Komplex der digitalen Musik- und Medienedition. &Uuml;ber Ergebnisse wird unter den thematischen Rubriken dieser Website berichtet, wobei die innerhalb dieser Forschungsarbeiten entwickelten Werkzeuge separat unter dem Punkt &bdquo;Werkzeuge&ldquo; beschrieben sind.</p><hr /><p><br /></p></ac:layout-cell><ac:layout-cell><p><br /></p></ac:layout-cell></ac:layout-section></ac:layout>',32571406,2),
+};
